0f45a86f1b3145a60a6e3d9084e26933fd9572d4
vedansh1209 <vedanshsingh1209@gmail.com>
2024-05-17 20:31:03 +0530

    i

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Contact from './Pages/Contact/Contact'
 import About from './Pages/AboutUs/AboutUs';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
+
 const router = createBrowserRouter([
     {
       path: "/",
