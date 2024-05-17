a270b23ff8cc478673b933c8b33f5da35adf7e7c
Vedansh <143615585+vedansh1209@users.noreply.github.com>
2024-05-17 18:34:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import MainPage from './Pages/MainPage/MainPage';
-import Contact from './Pages/Contact/Contact';
+import React from 'react'
+import MainPage from './Pages/MainPage/MainPage'
+import Contact from './Pages/Contact/Contact'
 import About from './Pages/AboutUs/AboutUs';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-const basePath = window.location.pathname;
 
 const router = createBrowserRouter([
-  {
-    path: `${basePath}/`,
-    element: <MainPage />,
-  },
-  {
-    path: `${basePath}/contact`,
-    element: <Contact />,
-  },
-  {
-    path: `${basePath}/about`,
-    element: <About />,
-  },
-]);
+    {
+      path: "/",
+      element: <MainPage/>,
+    },
+    {
+      path: "/contact",
+      element: <Contact/>,
+    },
+    {
+      path: "/about",
+      element: <About/>,
+    },
+  ]);
+
 
 function App() {
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+    return (
+        <div>
+            <RouterProvider router={router} />
+        </div>
+    )
 }
 
 export default App;
